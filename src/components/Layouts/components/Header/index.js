9bcccb0221146c7249c06510bc5ee2d78aa9bc63
root <khoigrai1@gmail.com>
2022-11-25 16:28:33 +0700

    fe_build_khôinguyễnđăngNov2520221626

--- src/components/Layouts/components/Header/index.js
+++ src/components/Layouts/components/Header/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable @typescript-eslint/no-empty-function */
@@ -13,7 +16,13 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import React, { useState } from 'react';
-import { menu } from '~/img';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
+import Dropdown from 'react-bootstrap/Dropdown';
+import { FaBeer, FaRegBell } from 'react-icons/fa';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Toast from 'react-bootstrap/Toast';
+import { avt, menu } from '~/img';
 import styles from './Header.module.css';
 
 function Header() {
@@ -21,6 +30,11 @@ function Header() {
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [showA, setShowA] = useState(false);
+
+  const toggleShowA = () => setShowA(!showA);
+  const hindeA = () => setShowA(false);
 
   return (
     <Navbar bg="light" expand="lg">
@@ -72,8 +86,66 @@ function Header() {
               aria-label="Search"
             />
 
-            <Button variant="outline-success">Search</Button>
+            <Button className={styles.search_btn} variant="outline-success">
+              Search
+            </Button>
           </Form>
+          <Row className={clsx(styles.grNoty)}>
+            <Col md={6} className="">
+              <Button
+                onClick={toggleShowA}
+                className={clsx(styles.bell, 'mb-s')}
+              >
+                <FaRegBell />
+              </Button>
+              <Toast
+                className={clsx(styles.notification)}
+                show={showA}
+                onClose={toggleShowA}
+              >
+                {[1, 2, 3].map(() => {
+                  return (
+                    <Toast>
+                      <Toast.Header closeButton={false}>
+                        <img
+                          src="holder.js/20x20?text=%20"
+                          className="rounded me-2"
+                          alt=""
+                        />
+                        <strong className="me-auto">Bootstrap</strong>
+                        <small>11 mins ago</small>
+                      </Toast.Header>
+                      <Toast.Body>
+                        Woohoo, you're reading this text in a Toast!
+                      </Toast.Body>
+                    </Toast>
+                  );
+                })}
+              </Toast>
+            </Col>
+          </Row>
+          <div
+            onClick={() => {
+              hindeA();
+            }}
+            className={clsx(styles.user)}
+          >
+            <Dropdown className={clsx(styles.avtDropDown)}>
+              <Dropdown.Toggle
+                className={clsx(styles.realAvt)}
+                split
+                variant="success"
+                id="dropdown-split-basic"
+              >
+                <img className={clsx(styles.avt)} src={avt} alt="" />
+              </Dropdown.Toggle>
+              <Dropdown.Menu className={clsx(styles.dropInfo)}>
+                <Dropdown.Item href="/profile">My profile</Dropdown.Item>
+                <Dropdown.Item href="#/action-2">sth</Dropdown.Item>
+                <Dropdown.Item href="/login">Log out</Dropdown.Item>
+              </Dropdown.Menu>
+            </Dropdown>
+          </div>
         </Navbar.Collapse>
       </Container>
     </Navbar>
